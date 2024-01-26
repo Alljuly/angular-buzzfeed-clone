4771a9fd59c4ca6f1b024715023a0fcaabdbd8f9
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-25 21:39:59 -0300

    arquivos base

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css'],
+})
+export class QuizzComponent implements OnInit {
+  ngOnInit(): void {}
+}
